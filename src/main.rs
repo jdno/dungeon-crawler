231f3de5ef2133008148d10b07c5fb115ea31314
Jan David <jdno@jdno.dev>
2022-02-23 14:39:47 +0100

    Initialize map module with constants for screen size

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+mod map;
+
+const SCREEN_HEIGHT: i32 = 50;
+const SCREEN_WIDTH: i32 = 80;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/map.rs
@@ -0,0 +1,3 @@
+use crate::{SCREEN_HEIGHT, SCREEN_WIDTH};
+
+const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
