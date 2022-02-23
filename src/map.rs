4a28af3c4736757313321598b8a911e7c474c5d8
Jan David <jdno@jdno.dev>
2022-02-23 14:42:09 +0100

    Create map struct

--- src/map.rs
+++ src/map.rs
@@ -1,3 +1,21 @@
 use crate::{SCREEN_HEIGHT, SCREEN_WIDTH};
 
 const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
+
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+pub enum TileType {
+    Floor,
+    Wall,
+}
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
+}
+
+impl Map {
+    pub fn new() -> Self {
+        Self {
+            tiles: vec![TileType::Floor; NUM_TILES],
+        }
+    }
+}
