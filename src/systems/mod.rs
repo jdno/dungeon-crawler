133f828c8c4038104fa8681936b42be1a9113cc4
Jan David <jdno@jdno.dev>
2022-02-24 13:54:31 +0100

    Create system that removes enemies upon collision

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,11 +1,13 @@
 use legion::Schedule;
 
+mod collisions;
 mod process_input;
 mod render_entity;
 mod render_map;
 
 pub fn init_scheduler() -> Schedule {
     Schedule::builder()
+        .add_system(collisions::collisions_system())
         .add_system(process_input::process_input_system())
         .add_system(render_entity::render_entity_system())
         .add_system(render_map::render_map_system())
