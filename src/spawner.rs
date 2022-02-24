ce49e57d879658217f825cbf754f92104da17f1f
Jan David <jdno@jdno.dev>
2022-02-24 13:41:31 +0100

    Spawn random monster in each room

--- src/spawner.rs
+++ src/spawner.rs
@@ -1,7 +1,23 @@
 use bracket_lib::prelude::*;
 use legion::World;
 
-use crate::components::{Player, Render};
+use crate::components::{Enemy, Player, Render};
+
+pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, position: Point) {
+    ecs.push((
+        Enemy,
+        position,
+        Render {
+            color: ColorPair::new(WHITE, BLACK),
+            glyph: match rng.range(0, 4) {
+                0 => to_cp437('E'),
+                1 => to_cp437('O'),
+                2 => to_cp437('o'),
+                _ => to_cp437('g'),
+            },
+        },
+    ));
+}
 
 pub fn spawn_player(ecs: &mut World, position: Point) {
     ecs.push((
