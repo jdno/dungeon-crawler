a83fb31106be284371660c753ef16944dc5e1acc
Jan David <jdno@jdno.dev>
2022-02-25 13:15:16 +0100

    Send intent messages to move player

--- src/systems/process_input.rs
+++ src/systems/process_input.rs
@@ -1,18 +1,18 @@
 use bracket_lib::prelude::*;
+use legion::systems::CommandBuffer;
 use legion::world::SubWorld;
-use legion::{component, system, IntoQuery};
+use legion::{component, system, Entity, IntoQuery};
 
-use crate::components::Player;
-use crate::{Camera, Map, TurnState};
+use crate::components::{Player, WantsToMove};
+use crate::TurnState;
 
 #[system]
 #[read_component(Player)]
-#[write_component(Point)]
+#[read_component(Point)]
 pub fn process_input(
     ecs: &mut SubWorld,
-    #[resource] map: &Map,
+    commands: &mut CommandBuffer,
     #[resource] key: &Option<VirtualKeyCode>,
-    #[resource] camera: &mut Camera,
     #[resource] turn_state: &mut TurnState,
 ) {
     if let Some(key) = key {
@@ -24,18 +24,21 @@ pub fn process_input(
             _ => Point::zero(),
         };
 
-        if delta.x != 0 || delta.y != 0 {
-            let mut players = <&mut Point>::query().filter(component::<Player>());
-
-            players.iter_mut(ecs).for_each(|position| {
+        <(Entity, &Point)>::query()
+            .filter(component::<Player>())
+            .iter_mut(ecs)
+            .for_each(|(entity, position)| {
                 let destination = *position + delta;
 
-                if map.is_enterable_tile(destination) {
-                    *position = destination;
-                    camera.on_player_move(destination);
-                    *turn_state = TurnState::PlayerTurn;
-                }
-            })
-        }
+                commands.push((
+                    (),
+                    WantsToMove {
+                        entity: *entity,
+                        destination,
+                    },
+                ));
+            });
+
+        *turn_state = TurnState::PlayerTurn;
     }
 }
