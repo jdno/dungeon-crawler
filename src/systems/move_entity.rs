33f93ecb76b46d3ef8ba5f5dce5c537598ce2217
Jan David <jdno@jdno.dev>
2022-02-28 17:14:01 +0100

    Update field of fiew when entities move

--- src/systems/move_entity.rs
+++ src/systems/move_entity.rs
@@ -2,11 +2,12 @@ use legion::systems::CommandBuffer;
 use legion::world::SubWorld;
 use legion::{system, Entity, EntityStore};
 
-use crate::components::Player;
 use crate::components::WantsToMove;
+use crate::components::{FieldOfView, Player};
 use crate::{Camera, Map};
 
 #[system(for_each)]
+#[read_component(FieldOfView)]
 #[read_component(Player)]
 pub fn move_entity(
     ecs: &SubWorld,
@@ -19,14 +20,15 @@ pub fn move_entity(
     if map.is_enterable_tile(want_move.destination) {
         commands.add_component(want_move.entity, want_move.destination);
 
-        if ecs
-            .entry_ref(want_move.entity)
-            .unwrap()
-            .get_component::<Player>()
-            .is_ok()
-        {
-            camera.on_player_move(want_move.destination);
+        if let Ok(entry) = ecs.entry_ref(want_move.entity) {
+            if let Ok(fov) = entry.get_component::<FieldOfView>() {
+                commands.add_component(want_move.entity, fov.clone_dirty());
+            }
+            if entry.get_component::<Player>().is_ok() {
+                camera.on_player_move(want_move.destination);
+            }
         }
     }
+
     commands.remove(*entity);
 }
