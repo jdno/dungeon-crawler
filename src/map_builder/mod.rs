d3b52cfaaac3b716bf97cdb12ffb15a8faa98de5
Jan David <jdno@jdno.dev>
2022-03-03 18:26:02 +0100

    Pick map architect at random

--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -33,7 +33,12 @@ pub struct MapBuilder {
 
 impl MapBuilder {
     pub fn new(rng: &mut RandomNumberGenerator) -> Self {
-        let mut architect = DrunkardsWalkArchitect {};
+        let mut architect: Box<dyn MapArchitect> = match rng.range(0, 3) {
+            0 => Box::new(DrunkardsWalkArchitect {}),
+            1 => Box::new(RoomsArchitect {}),
+            _ => Box::new(CellularAutomataArchitect {}),
+        };
+
         architect.build(rng)
     }
 
